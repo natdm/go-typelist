fb0df694db55901a22366027acfbfe2c6ae29e24
Nathan Hyland <nathan.hyland@gmail.com>
2017-05-15 10:54:10 -0700

    Added more type support

--- main.go
+++ main.go
@@ -16,11 +16,10 @@ import (
 
 // Object represents a Go type
 type Object struct {
-	Line      int    `json:"line"`
-	Name      string `json:"name"`
-	Comment   string `json:"comment"`
-	Signature string `json:"signature"`
-	Type      string `json:"type"`
+	Line      int     `json:"line"`
+	Signature string  `json:"signature"`
+	Type      string  `json:"type"`
+	Name      *string `json:"name"`
 }
 
 // ObjectsVersion has the version of the package as well as the objects
@@ -37,99 +36,48 @@ func main() {
 		os.Exit(0)
 	}
 
-	out := []Object{}
+	// map the line number to the object
+	out := map[int]*Object{}
 	fset := token.NewFileSet()
-	file, e := parser.ParseFile(fset, os.Args[1], nil, parser.AllErrors)
-	if e != nil {
-		log.Fatal(e)
-		return
+	file, err := parser.ParseFile(fset, os.Args[1], nil, parser.AllErrors)
+	if err != nil {
+		log.Fatal(err)
 	}
 	bs, err := ioutil.ReadFile(os.Args[1])
 	if err != nil {
-		log.Fatal(e)
+		log.Fatal(err)
 	}
 
+	// get all signatures, types, and lines and add them to the map
 	for i := range file.Decls {
 		o := inspectNode(file.Decls[i], bs, fset)
-		if o.Type == "MethodDecl" {
-			out = append(out, o)
+		switch o.Type {
+		case "":
+			continue
+		default:
+			out[o.Line] = &o
 		}
-
 	}
 
-	for k, v := range file.Scope.Objects {
-		fld, ok := v.Decl.(*ast.Field)
-		if ok {
-			obj := Object{
-				Name:      k,
-				Signature: string(bs[fld.Pos()-1 : fld.End()]),
-				Line:      fset.File(v.Pos()).Line(v.Pos()),
-			}
-			out = append(out, obj)
-			continue
+	// collect the names of these types for syntax highlighting.
+	for _, v := range file.Scope.Objects {
+		switch v.Decl.(type) {
+		case *ast.Field,
+			*ast.SelectorExpr,
+			*ast.TypeSpec,
+			*ast.FuncDecl,
+			*ast.FuncLit,
+			*ast.FuncType:
+			out[fset.File(v.Pos()).Line(v.Pos())].Name = &v.Name
+		default:
 		}
-
-		se, ok := v.Decl.(*ast.SelectorExpr)
-		if ok {
-			obj := Object{
-				Name:      k,
-				Signature: string(bs[se.Pos()-1 : se.End()]),
-				Line:      fset.File(v.Pos()).Line(v.Pos()),
-			}
-			out = append(out, obj)
-			continue
-		}
-
-		ts, ok := v.Decl.(*ast.TypeSpec)
-		if ok {
-			obj := Object{
-				Name:      k,
-				Line:      fset.File(v.Pos()).Line(v.Pos()),
-				Signature: string(bs[ts.Pos()-1 : ts.End()]),
-				Type:      "TypeSpec",
-			}
-			out = append(out, obj)
-			continue
-		}
-
-		fd, ok := v.Decl.(*ast.FuncDecl)
-		if ok {
-			out = append(out, Object{
-				Type:      "FuncDecl",
-				Name:      k,
-				Signature: string(bs[fd.Type.Pos()-1 : fd.Type.End()]),
-				Line:      fset.File(v.Pos()).Line(v.Pos()),
-				Comment:   "I am a comment",
-			})
-			continue
-		}
-
-		fl, ok := v.Decl.(*ast.FuncLit)
-		if ok {
-			out = append(out, Object{
-				Type:      "FuncLit",
-				Name:      k,
-				Signature: string(bs[fl.Pos()-1 : fl.End()]),
-				Line:      fset.File(v.Pos()).Line(v.Pos()),
-				Comment:   "I am a comment",
-			})
-			continue
-		}
-
-		ft, ok := v.Decl.(*ast.FuncType)
-		if ok {
-			out = append(out, Object{
-				Type:      "FuncType",
-				Name:      k,
-				Signature: string(bs[ft.Pos()-1 : ft.End()]),
-				Line:      fset.File(v.Pos()).Line(v.Pos()),
-				Comment:   "I am a comment",
-			})
-			continue
-		}
-
 	}
-	objV := ObjectsVersion{Version: version, Objects: out}
+
+	objArr := []Object{}
+	for _, v := range out {
+		objArr = append(objArr, *v)
+	}
+	objV := ObjectsVersion{Version: version, Objects: objArr}
 	outBs, err := json.MarshalIndent(objV, "", "\t")
 	if err != nil {
 		log.Fatal(err)
@@ -149,15 +97,56 @@ func inspectNode(node ast.Node, bs []byte, fset *token.FileSet) Object {
 			body := string(bs[x.Pos()-1 : x.End()])
 			index := strings.Index(body, "{\n\treturn")
 			sig := body[:index-1]
-			obj.Name = x.Name.String()
 			obj.Signature = sig
 			obj.Line = fset.File(x.Pos()).Line(x.Pos())
 			obj.Type = "MethodDecl"
 		case *ast.ChanType:
-			obj.Name = ""
-			obj.Signature = string(bs[x.Pos()-1 : x.End()])
+			obj.Signature = string(bs[node.Pos()-1 : node.End()])
 			obj.Line = fset.File(x.Pos()).Line(x.Pos())
 			obj.Type = "ChanType"
+		case *ast.DeclStmt:
+			body := string(bs[x.Pos()-1 : x.End()])
+			index := strings.Index(body, "{\n")
+			sig := body[:index-1]
+			z := n.(*ast.DeclStmt)
+			obj.Signature = sig
+			obj.Line = fset.File(z.Pos()).Line(z.Pos())
+			obj.Type = "DeclStmt"
+		case *ast.FuncLit:
+			body := string(bs[node.Pos()-1 : node.End()])
+			index := strings.Index(body, "{\n")
+			sig := body[:index-1]
+			z := n.(*ast.FuncLit)
+			obj.Signature = sig
+			obj.Line = fset.File(z.Type.Pos()).Line(z.Type.Pos())
+			obj.Type = "FuncLit"
+		case *ast.FuncType:
+			body := string(bs[node.Pos()-1 : node.End()])
+			index := strings.Index(body, "{")
+			var sig string
+			if index > -1 {
+				sig = body[:index-1]
+			} else {
+				sig = body
+			}
+			obj.Signature = sig
+			obj.Line = fset.File(x.Pos()).Line(x.Pos())
+			obj.Type = "FuncType"
+		case *ast.StructType:
+			body := string(bs[node.Pos()-1 : node.End()])
+			index := strings.Index(body, "struct")
+			sig := body[:index+6]
+			obj.Signature = sig
+			obj.Line = fset.File(x.Pos()).Line(x.Pos())
+			obj.Type = "StructType"
+		case *ast.TypeSpec:
+			obj.Signature = string(bs[node.Pos()-1 : node.End()])
+			obj.Line = fset.File(x.Pos()).Line(x.Pos())
+			obj.Type = "TypeSpec"
+		case *ast.GenDecl:
+			obj.Signature = string(bs[node.Pos()-1 : node.End()])
+			obj.Line = fset.File(x.Pos()).Line(x.Pos())
+			obj.Type = "TypeSpec"
 		default:
 		}
 		return true
--- tests/test.go
+++ tests/test.go
@@ -5,10 +5,11 @@ type MyStructType struct {
 	data2 int
 }
 
-type MyInterfaceType interface {
-	Read()
-	Write()
-}
+//
+// type MyInterfaceType interface {
+// 	Read()
+// 	Write()
+// }
 
 const (
 	const1 = iota + 1
@@ -16,42 +17,55 @@ const (
 )
 
 type thing []string
+type fixedArr [2]string
 
-// SomeAwesomefunc is awesome
-func SomeAwesomefunc(s string, y *string) (string, error) {
-	return "", nil
+func HELLOFROMAFUNC(s string) {
+
 }
+
+//
+// // SomeAwesomefunc is awesome
+// func SomeAwesomefunc(s string, y *string) (string, error) {
+// 	return "", nil
+// }
 
 var myFunc = func(s string) {
 
 }
 
+//
 type SomeFuncDeclMaybe func(s string)
 
+//
 type ch chan string
 
+//
 type ch2 chan<- string
+
 type ch3 <-chan string
 
-// TakesAChannel is a bit more complex.
-func TakesAChannel(in <-chan string, something bool, done chan struct{}) error {
-	return nil
-}
+type Whatever map[string]string
 
-// MyType has methods
-type MyTypeStr struct {
-}
-
-// String satisfies stringer
-func (t *MyTypeStr) String() string {
-	return "Hello"
-}
-
-// StringNoPtr has no ptr receiver
-func (t MyTypeStr) StringNoPtr() string {
-	return "Hello"
-}
-
-func TestFunc() {
-
-}
+//
+// // TakesAChannel is a bit more complex.
+// func TakesAChannel(in <-chan string, something bool, done chan struct{}) error {
+// 	return nil
+// }
+//
+// // MyType has methods
+// type MyTypeStr struct {
+// }
+//
+// // String satisfies stringer
+// func (t *MyTypeStr) String() string {
+// 	return "Hello"
+// }
+//
+// // StringNoPtr has no ptr receiver
+// func (t MyTypeStr) StringNoPtr() string {
+// 	return "Hello"
+// }
+//
+// func TestFunc() {
+//
+// }
